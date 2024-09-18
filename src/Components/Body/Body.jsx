d5fa2915a42a4fe054049c48cfcf8437a6695b88
Pablo Martín Escudero <escuderopablo@hotmail.com.ar>
2024-09-17 23:00:07 -0300

    Add: fuente desde google fonts

--- src/Components/Body/Body.jsx
+++ src/Components/Body/Body.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import About from '../About/About';
 import Projects from '../Projects/Projects';
 import Contact from '../Contact/Contact';
+import '../../styles/body.css';
 
 const Body = () => {
   return (
-    <div>
+    <div className={'bodyContainer'}>
       <section id="about">
         <About />
       </section>
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,19 +1,26 @@
-import {React} from 'react';
-import '../../styles/header.css'
+import { React } from 'react';
+import '../../styles/header.css';
 
 const Header = () => {
-    return (
-      <header className={"mainHeader"}>
-        <h1>Escudero Pablo Martín</h1>
-        <nav>
-          <ul>
-            <li><a href="#about">Sobre mí</a></li>
-            <li><a href="#projects">Proyectos</a></li>
-            <li><a href="#contact">Contacto</a></li>
-          </ul>
-        </nav>
-      </header>
-    );
-  }
+  return (
+    <header className={'mainHeader'}>
+      <h1 className={'mainTitle'}>{'Escudero Pablo Martín'}</h1>
+      <h2 className={'mainSubTitle'}>{'Desarrollador'}</h2>
+      <nav>
+        <ul>
+          <li>
+            <a href="#about">Sobre mí</a>
+          </li>
+          <li>
+            <a href="#projects">Proyectos</a>
+          </li>
+          <li>
+            <a href="#contact">Contacto</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
