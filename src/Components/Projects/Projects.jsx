7f91ad6ae184178152af9f8371bdd8e6be094476
Pablo Martín Escudero <escuderopablo@hotmail.com.ar>
2024-09-18 01:32:47 -0300

    Se elimina coonsumo api - add hardcode repos

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,23 +1,73 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import RepoView from './RepoView';
 import '../../styles/projects.css';
+import gitTeam from '../../assets/team.png';
+import person from '../../assets/person.png';
+import Spinner from '../../Commons/Spinner';
 
 const Projects = () => {
+  const [delayed, setDelayed] = useState(true);
+
+  //Dado que no tengo backend, no puedo hacer llamados a la api, ya que requiere token después de x cantidad de
   const repos = [
-    'UTNProgramacion3/tp-winform-equipo-16A',
-    'escu-git/escuderopablomartin',
-    'escu-git/notitas-api',
-    'escu-git/notitas_ui',
-    'escu-git/irwins_revenge_game',
-    // 'escu-git/wallet-dashboard',
+    {
+      url: 'UTNProgramacion3/tp-winform-equipo-16A',
+      name: 'tp-winform-equipo-16A',
+      img: gitTeam,
+      description: 'Aplicación para gestión de artículos de comercio',
+      lenguajes: 'C#',
+      creado: '9/4/2024',
+    },
+    {
+      url: 'escu-git/escuderopablomartin',
+      name: 'escuderopablomartin',
+      img: person,
+      description: 'Portfolio realizado para la materia programación 3, UTN.',
+      lenguajes: 'HTML',
+      creado: '9/17/2024',
+    },
+    {
+      url: 'escu-git/notitas-api',
+      name: 'notitas-api',
+      img: person,
+      description: 'Api Notitas App',
+      lenguajes: 'JavaScript',
+      creado: '1/18/2024',
+    },
+    {
+      url: 'escu-git/notitas_ui',
+      name: 'notitas_ui',
+      img: person,
+      description: 'Front Notitas App',
+      lenguajes: 'TypeScript',
+      creado: '1/20/2024',
+    },
+    {
+      url: 'escu-git/irwins_revenge_game',
+      name: 'irwins_revenge_game',
+      img: person,
+      description:
+        "Juego de consola Irwin's Revenge. Segundo parcial de la carrera Tecnicatura Universitaria en Programación, UTN.",
+      lenguajes: 'C++',
+      creado: '6/3/2023',
+    },
   ];
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDelayed(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (delayed) return <Spinner />;
   return (
     <div className="projects-container">
       <h2 className={'projects-title'}>Proyectos</h2>
       <div className="projects">
         {repos.map((repo, index) => (
-          <RepoView key={index} repoUrl={repo} />
+          <RepoView key={index} repoData={repo} />
         ))}
       </div>
     </div>
--- src/Components/Projects/RepoView.jsx
+++ src/Components/Projects/RepoView.jsx
@@ -1,60 +1,26 @@
 import React, { useEffect, useState } from 'react';
 
-const RepoView = ({ repoUrl }) => {
-  const [repoData, setRepoData] = useState(null);
-
-  useEffect(() => {
-    const fetchRepoData = async () => {
-      try {
-        const response = await fetch(
-          `https://api.github.com/repos/${repoUrl}`,
-          {
-            headers: {
-              Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
-              Accept: 'application/vnd.github.v3+json',
-            },
-          }
-        );
-
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-
-        const data = await response.json();
-        setRepoData(data);
-      } catch (error) {
-        console.error('Error fetching repository data:', error);
-      }
-    };
-
-    fetchRepoData();
-  }, [repoUrl]);
-
-  if (!repoData) return <p>Loading...</p>;
+const RepoView = ({ repoData }) => {
+  const { url, img, description, lenguajes, creado, name } = repoData;
 
   return (
-    <div className="repo-details">
+    <div className="repo-details repo-card">
       <div className="repo-header">
-        <img
-          src={repoData.owner.avatar_url}
-          alt={`${repoData.owner.login} avatar`}
-          className="owner-avatar"
-        />
-        <h1>{repoData.name}</h1>
+        <img src={img} alt={'imagen'} className="owner-avatar" />
+        <h1>{name}</h1>
       </div>
-      <p>{repoData.description}</p>
+      <p>{description}</p>
       <p>
-        <strong>Lenguaje:</strong> {repoData.language}
+        <strong>Lenguaje:</strong> {lenguajes}
       </p>
       <p>
-        <strong>Creado:</strong>{' '}
-        {new Date(repoData.created_at).toLocaleDateString()}
+        <strong>Creado:</strong> {new Date(creado).toLocaleDateString()}
       </p>
-      <p>
-        <strong>Última actualización:</strong>{' '}
-        {new Date(repoData.updated_at).toLocaleDateString()}
-      </p>
-      <a href={repoData.html_url} target="_blank" rel="noopener noreferrer">
+      <a
+        href={`https://github.com/${url}`}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         Ver en GitHub
       </a>
     </div>
